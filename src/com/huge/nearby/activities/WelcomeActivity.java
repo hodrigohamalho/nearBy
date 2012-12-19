30ab3e2f6377c803dd019cc0a3544b6b58341584
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2012-12-19 03:45:37 -0200

    fixed ClassNotFound error and made interface of welcome page

--- src/com/huge/nearby/activities/WelcomeActivity.java
+++ src/com/huge/nearby/activities/WelcomeActivity.java
@@ -1,17 +1,21 @@
-package com.hugeinc.nearby.activities;
+package com.huge.nearby.activities;
 
-import com.hugeinc.nearby.R;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+
+import com.huge.nearby.R;
 
 public class WelcomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		removeTitleBar();
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		
 	}
 
 	@Override
@@ -19,6 +23,10 @@ public class WelcomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_welcome, menu);
 		return true;
+	}
+	
+	private void removeTitleBar(){
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 	}
 
 }
