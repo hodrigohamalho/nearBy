a7fdc60a37c3e17b5f587d5f810fc8e88ffb114f
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2012-12-26 11:27:54 -0200

    Welcome window

--- src/com/huge/nearby/activities/WelcomeActivity.java
+++ src/com/huge/nearby/activities/WelcomeActivity.java
@@ -1,9 +1,12 @@
 package com.huge.nearby.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 
 import com.huge.nearby.R;
 
@@ -16,8 +19,25 @@ public class WelcomeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
 		
+		handleNearbyButton();
 	}
 
+	private void handleNearbyButton() {
+		ImageButton nearByBtn = (ImageButton) findViewById(R.id.nearBy_btn);
+		nearByBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent nearByIntent = new Intent(NearbyActivity.class.getCanonicalName());
+				startActivity(nearByIntent);
+			}
+		});
+	}
+
+	private void removeTitleBar(){
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -25,8 +45,4 @@ public class WelcomeActivity extends Activity {
 		return true;
 	}
 	
-	private void removeTitleBar(){
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-	}
-
 }
